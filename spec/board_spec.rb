d6bfcc68e42c8908a508319743c4b5258351eff3
RobbieS-coder <127651345+RobbieS-coder@users.noreply.github.com>
2024-08-04 08:28:36 +0100

    Write tests for code yet to come

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -30,21 +30,25 @@ describe Board do
 
     context 'when moving knight into occupied square of same colour' do
       xit 'returns false' do
+        expect(board.valid_move?('g1e2', 'white')).to be(false)
       end
     end
 
     context 'when moving bishop into occupied square of same colour' do
       xit 'returns false' do
+        expect(board.valid_move?('f1e2', 'white')).to be(false)
       end
     end
 
     context 'when moving rook into occupied square of same colour' do
       xit 'returns false' do
+        expect(board.valid_move?('h1h2', 'white')).to be(false)
       end
     end
 
     context 'when moving queen into occupied square of same colour' do
       xit 'returns false' do
+        expect(board.valid_move?('d1d2', 'white')).to be(false)
       end
     end
 
@@ -116,113 +120,243 @@ describe Board do
       end
     end
 
-    context 'when moving knight in multiple valid directions' do
+    context 'when moving knight two up then one across' do
       xit 'returns true' do
+        expect(board.valid_move?('g1f3', 'white')).to be(true)
+      end
+    end
+
+    context 'when moving knight one up then two across' do
+      before do
+        setup_moves = %w[g1f3 a7a6]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
+      xit 'returns true' do
+        expect(board.valid_move?('f3d4', 'white')).to be(true)
+      end
+    end
+
+    context 'when moving knight two down then one across' do
+      before do
+        setup_moves = %w[g1f3 a7a6]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
+      xit 'returns true' do
+        expect(board.valid_move?('f3g1', 'white')).to be(true)
+      end
+    end
+
+    context 'when moving knight one down then two across' do
+      before do
+        setup_moves = %w[g1f3 a7a6 f3d4 a6a5]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
+      xit 'returns true' do
+        expect(board.valid_move?('d4f3', 'white')).to be(true)
       end
     end
 
     context 'when moving knight two squares' do
       xit 'returns false' do
+        expect(board.valid_move?('g1g3', 'white')).to be(false)
       end
     end
 
     context 'when moving knight to take piece' do
+      before do
+        setup_moves = %w[g1f3 e7e5]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
       xit 'returns true' do
+        expect(board.valid_move?('f3e5p', 'white')).to be(true)
       end
     end
 
     context 'when moving bishop diagonally without blocked path' do
+      before do
+        setup_moves = %w[e2e4 a7a6]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
       xit 'returns true' do
+        expect(board.valid_move?('f1c4', 'white')).to be(true)
       end
     end
 
     context 'when moving bishop diagonally with blocked path' do
       xit 'returns false' do
+        expect(board.valid_move?('f1c4', 'white')).to be(false)
       end
     end
 
     context 'when moving bishop vertically' do
+      before do
+        setup_moves = %w[f2f4 a7a6]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
       xit 'returns false' do
+        expect(board.valid_move?('f1f3', 'white')).to be(false)
       end
     end
 
     context 'when moving bishop horizontally' do
+      before do
+        setup_moves = %w[g1f3 a7a6]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
       xit 'returns false' do
+        expect(board.valid_move?('f1g1', 'white')).to be(false)
       end
     end
 
     context 'when moving bishop to take piece' do
+      before do
+        setup_moves = %w[e2e4 b7b5]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
       xit 'returns true' do
+        expect(board.valid_move?('f1b5p', 'white')).to be(true)
       end
     end
 
     context 'when moving rook vertically without blocked path' do
+      before do
+        setup_moves = %w[h2h4 a7a6]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
       xit 'returns true' do
+        expect(board.valid_move?('h1h3', 'white')).to be(true)
       end
     end
 
     context 'when moving rook vertically with blocked path' do
       xit 'returns false' do
+        expect(board.valid_move?('h1h3', 'white')).to be(false)
       end
     end
 
     context 'when moving rook horizontally without blocked path' do
+      before do
+        setup_moves = %w[g1f3 a7a6]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
       xit 'returns true' do
+        expect(board.valid_move?('h1g1', 'white')).to be(true)
       end
     end
 
     context 'when moving rook horizontally with blocked path' do
+      before do
+        setup_moves = %w[b1c3 a7a6]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
       xit 'returns false' do
+        expect(board.valid_move?('h1b1', 'white')).to be(false)
       end
     end
 
     context 'when moving rook diagonally' do
+      before do
+        setup_moves = %w[g2g4 a7a6]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
       xit 'returns false' do
+        expect(board.valid_move?('h1e4', 'white')).to be(false)
       end
     end
 
     context 'when moving rook to take piece' do
+      before do
+        setup_moves = %w[h2h4 g7g5 h4g5p h7h5]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
       xit 'returns true' do
+        expect(board.valid_move?('h1h5p', 'white')).to be(true)
       end
     end
 
     context 'when moving queen vertically without blocked path' do
+      before do
+        setup_moves = %w[d2d4 a7a6]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
       xit 'returns true' do
+        expect(board.valid_move?('d1d3', 'white')).to be(true)
       end
     end
 
     context 'when moving queen vertically with blocked path' do
       xit 'returns true' do
+        expect(board.valid_move?('d1d3', 'white')).to be(true)
       end
     end
 
     context 'when moving queen horizontally without blocked path' do
+      before do
+        setup_moves = %w[d2d4 a7a6 d1d3 a6a5]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
       xit 'returns true' do
+        expect(board.valid_move?('d3h3', 'white')).to be(true)
       end
     end
 
     context 'when moving queen horizontally with blocked path' do
+      before do
+        setup_moves = %w[b1c3 a7a6]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
       xit 'returns true' do
+        expect(board.valid_move?('d1b1', 'white')).to be(true)
       end
     end
 
     context 'when moving queen diagonally without blocked path' do
+      before do
+        setup_moves = %w[e2e4 a7a6]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
       xit 'returns true' do
+        expect(board.valid_move?('d1f3', 'white')).to be(true)
       end
     end
 
     context 'when moving queen diagonally with blocked path' do
       xit 'returns true' do
+        expect(board.valid_move?('d1f3', 'white')).to be(true)
       end
     end
 
     context 'when moving queen like a knight' do
       xit 'returns false' do
+        expect(board.valid_move?('d1e3', 'white')).to be(false)
       end
     end
 
     context 'when moving queen to take piece' do
+      before do
+        setup_moves = %w[d2d4 e7e5 d4e5p d7d5]
+        setup_moves.each { |move| board.update_board(move) }
+      end
+
       xit 'returns true' do
+        expect(board.valid_move?('d1d5p', 'white')).to be(true)
       end
     end
 
